7b6e25a9d7da5f862f18b36066b1cd7296a67e20
GUSTAV0-CRUZ <gustavocruzsantos39@gmail.com>
2025-11-10 13:54:14 -0300

    feat(alunos com fotos): lista um aluno com suas fotos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -41,14 +41,16 @@ class AlunoControllers {
   async show(req, res) {
     console.log('aluno show');
     try {
-      const user = await AlunoModel.findByPk(req.params.id);
-      if (!user) return res.status(400).json({ errors: ['Usuário não encontrado'] });
-      const {
-        id, nome, sobrenome, email, idade, altura, peso,
-      } = user;
-      return res.status(200).json({
-        id, nome, sobrenome, email, idade, altura, peso,
+      const user = await AlunoModel.findByPk(req.params.id, {
+        attributes: ['id', 'nome', 'sobrenome', 'email', 'idade', 'altura', 'peso'],
+        order: [['id', 'DESC'], [Picture, 'id', 'DESC']],
+        include: {
+          model: Picture,
+          attributes: ['id', 'originalname', 'filename'],
+        },
       });
+      if (!user) return res.status(400).json({ errors: ['Usuário não encontrado'] });
+      return res.status(200).json(user);
     } catch (e) {
       if (!e.errors) return res.status(400).json({ errors: ['Error não identificado'] });
       const erros = e.errors.map((erro) => erro.message);
